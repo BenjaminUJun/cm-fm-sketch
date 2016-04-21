df476814c2a63c08d1b8c97eff2e3b9393265d20
Hooman <hoomanm@princeton.edu>
2016-04-21 14:24:12 -0400

    Test script to validate the result of sketch

---
+++ testSketch.py
@@ -0,0 +1,51 @@
+class SrcDest(object):
+
+    def __init__(self, srcAddr,DestSet):
+        self.DestSet = DestSet
+        self.srcAddr = srcAddr
+        self.match = self
+    def __eq__(self, other):
+        result = (self.srcAddr == other.srcAddr)
+        if result:
+            self.match = other
+            other.DestSet = self.DestSet | other.DestSet
+            #print "Match: new self length" + str(len(other.DestSet))
+            #print "updateSet: new " + str(other.DestSet)
+        return result
+    def __ne__(self, other):
+        return self.srcAddr != other.srcAddr 
+    def __hash__(self):
+        return hash(self.srcAddr)
+
+from sets import Set
+import sys
+
+
+if __name__ == '__main__':
+
+
+    entireSet = Set()
+    
+    f = sys.argv[1]
+    with open(f, 'r') as fin:
+        for line in fin:
+            arr = line.strip().split(',') # [src, dst]
+            dest1 = Set([arr[1]])
+            sd1 = SrcDest(arr[0],dest1)
+
+            if sd1 in entireSet:
+                #print "--------Main Loop--------"
+                #print "existing " + str(sd1.DestSet)
+                #print "new " + str(dest1)
+                #print "Updated length " + str(len(sd1.DestSet))
+                #print "------End Main Loop------"
+                entireSet.discard(sd1)
+                entireSet.add(sd1)
+                #print sd1.match.DestSet
+            else:
+                entireSet.add(sd1)
+
+    for src in entireSet:
+        print src.srcAddr + " " + str(len(src.DestSet))
+        #for dst in src.DestSet:
+        #    print dst + ","

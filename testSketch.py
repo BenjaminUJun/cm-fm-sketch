975ae7096147b259c18221468135f2838572e791
Hooman <hoomanm@princeton.edu>
2016-05-04 14:34:32 -0400

    update testSketch.py

--- testSketch.py
+++ testSketch.py
@@ -27,8 +27,16 @@ if __name__ == '__main__':
     entireSet = Set()
     
     f = sys.argv[1]
+
+
     with open(f, 'r') as fin:
+        counter = 0
         for line in fin:
+
+            counter += 1
+            if (counter % 100000) == 0:
+                sys.stderr.write(str(counter//100000) + "%\n")
+
             arr = line.strip().split(',') # [src, dst]
             dest1 = Set([arr[1]])
             sd1 = SrcDest(arr[0],dest1)
@@ -45,7 +53,14 @@ if __name__ == '__main__':
             else:
                 entireSet.add(sd1)
 
+    k = sys.argv[2]
+    printDestNum = sys.argv[3]
     for src in entireSet:
-        print src.srcAddr + " " + str(len(src.DestSet))
+        #print src.srcAddr + " " + str(len(src.DestSet))
         #for dst in src.DestSet:
         #    print dst + ","
+        if len(src.DestSet) >= int(k):
+            if int(printDestNum) == 1:
+                print src.srcAddr + " " + str(len(src.DestSet))
+            else:
+                print src.srcAddr
